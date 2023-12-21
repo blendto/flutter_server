04d40b2b6187a2799ba971d5b94aac101bc7d6e6
souvikdas <souvik@blend.to>
2023-12-22 02:47:33 +0530

    Example app

---
+++ example/lib/main.dart
@@ -0,0 +1,48 @@
+import 'dart:ui' as ui;
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_server/video_container.dart';
+
+void main() {
+  runApp(MainApp());
+}
+
+class MainApp extends StatefulWidget {
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  Future<ui.Image> getImage(String key) async {
+    final byteData = await rootBundle.load(key);
+    return decodeImageFromList(byteData.buffer.asUint8List());
+  }
+
+  Future<(ui.Image, ui.Image)> getImages() async {
+    final hero = await getImage("assets/hero-1.png");
+    final diner = await getImage("assets/diner.jpg");
+    return (hero, diner);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Test',
+      home: FutureBuilder(
+          future: getImages(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            }
+            if (snapshot.hasError || snapshot.data == null) {
+              print(snapshot.error);
+              return Container(color: Colors.amber);
+            }
+            final hero = snapshot.data!.$1;
+            final bgData = snapshot.data!.$2;
+            return VideoContainer(hero: hero, bgData: bgData);
+          }),
+    );
+  }
+}
